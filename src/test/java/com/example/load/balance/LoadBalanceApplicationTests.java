58b03ce9355fb6b492695224394dcf590bc573e8
anil <anil@DESKTOP-D0JPL8G>
2020-11-05 13:20:12 +0530

    loadBalance Spring Boot

---
+++ src/test/java/com/example/load/balance/LoadBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.load.balance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
